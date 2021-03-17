2874c8205b85e272f2b93d12321467c1d83afbee
JCCLaude <56520720+JCCLaude@users.noreply.github.com>
2021-03-17 13:25:20 +0100

    Delete 01_Basic_basic directory

--- 01_Basic_basic/migrations/2_simple_smart_contract.js
+++
@@ -1,8 +0,0 @@
-//truffle runs the migration files in numeric order => 1 is for truffle, 2 is for us
-
-
-const SimpleSmartContract = artifacts.require("SimpleSmartContract");
-
-module.exports = function(deployer) {
-  deployer.deploy(SimpleSmartContract);
-};
