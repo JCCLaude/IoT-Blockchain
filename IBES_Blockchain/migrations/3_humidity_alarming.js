b19e21087f085f1addd13786c2c16302e51410d6
JCCLaude <56520720+JCCLaude@users.noreply.github.com>
2021-03-22 13:59:21 +0100

    Delete IBES_Blockchain directory

--- IBES_Blockchain/migrations/3_humidity_alarming.js
+++
@@ -1,5 +0,0 @@
-const Humidity_Alarming = artifacts.require("Humidity_Alarming");
-
-module.exports = function (deployer) {
-  deployer.deploy(Humidity_Alarming);
-};
--- IBES_Blockchain/migrations/4_CO2_alarming.js
+++
@@ -1,5 +0,0 @@
-const CO2_Alarming = artifacts.require("CO2_Alarming");
-
-module.exports = function (deployer) {
-  deployer.deploy(CO2_Alarming);
-};
