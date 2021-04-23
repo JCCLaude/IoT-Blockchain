6274d383777e24a4d85a21fc7314e757600e6ca6
JCCLaude <johannes.schuette@unibw.de>
2021-04-23 15:39:19 +0200

    updated LandingPage and Navbar
    1) Landing Page now implements error handling for a not connected blockchain
    2) The Navbar now does not cause reloads of the whole page when clicking links

--- Webserver/src/components/Blockchain_components/LandingPage.js
+++ Webserver/src/components/Blockchain_components/LandingPage.js
@@ -1,5 +1,5 @@
 import { React, useEffect, useState } from "react";
-import { Jumbotron, Container, Row } from "react-bootstrap";
+import { Jumbotron, Container, Row, Col } from "react-bootstrap";
 import CurrentStatus from "./CurrentStatus.js";
 
 import "./blockchain.css";
@@ -10,6 +10,7 @@ const TemperatureBuild = require("./assets/ethereumBuilds/Temperature_Alarming.j
 const HumidityBuild = require("./assets/ethereumBuilds/Humidity_Alarming.json");
 
 function LandingPage() {
+  const [error, setError] = useState(false);
   const [co2Event, setCo2Event] = useState({
     timestamp: "loading...",
     measurement: "loading...",
@@ -30,47 +31,52 @@ function LandingPage() {
   });
 
   const fetchEvents = async () => {
-    const web3 = new Web3("ws://192.168.1.88:7545");
-    const id = await web3.eth.net.getId();
-    const co2Contract = new web3.eth.Contract(
-      CO2Build.abi,
-      CO2Build.networks[id].address
-    );
-    const humidityContract = new web3.eth.Contract(
-      HumidityBuild.abi,
-      HumidityBuild.networks[id].address
-    );
-    const temperatureContract = new web3.eth.Contract(
-      TemperatureBuild.abi,
-      TemperatureBuild.networks[id].address
-    );
-    co2Contract
-      .getPastEvents("allEvents", {
-        fromBlock: 30,
-        toBlock: "latest",
-      })
-      .then((events) => {
-        const message = events[events.length - 1].returnValues;
-        setCo2Event(message);
-      });
-    humidityContract
-      .getPastEvents("allEvents", {
-        fromBlock: 30,
-        toBlock: "latest",
-      })
-      .then((events) => {
-        const message = events[events.length - 1].returnValues;
-        setHumidityEvent(message);
-      });
-    temperatureContract
-      .getPastEvents("allEvents", {
-        fromBlock: 30,
-        toBlock: "latest",
-      })
-      .then((events) => {
-        const message = events[events.length - 1].returnValues;
-        setTemperatureEvent(message);
-      });
+    try {
+      const web3 = new Web3("ws://192.168.1.88:7545");
+      const id = await web3.eth.net.getId();
+      const co2Contract = new web3.eth.Contract(
+        CO2Build.abi,
+        CO2Build.networks[id].address
+      );
+      const humidityContract = new web3.eth.Contract(
+        HumidityBuild.abi,
+        HumidityBuild.networks[id].address
+      );
+      const temperatureContract = new web3.eth.Contract(
+        TemperatureBuild.abi,
+        TemperatureBuild.networks[id].address
+      );
+      co2Contract
+        .getPastEvents("allEvents", {
+          fromBlock: 30,
+          toBlock: "latest",
+        })
+        .then((events) => {
+          const message = events[events.length - 1].returnValues;
+          setCo2Event(message);
+        });
+      humidityContract
+        .getPastEvents("allEvents", {
+          fromBlock: 30,
+          toBlock: "latest",
+        })
+        .then((events) => {
+          const message = events[events.length - 1].returnValues;
+          setHumidityEvent(message);
+        });
+      temperatureContract
+        .getPastEvents("allEvents", {
+          fromBlock: 30,
+          toBlock: "latest",
+        })
+        .then((events) => {
+          const message = events[events.length - 1].returnValues;
+          setTemperatureEvent(message);
+        });
+    } catch (error) {
+      console.log("No blockchain connection possible", error);
+      setError(true);
+    }
   };
 
   useEffect(() => {
@@ -92,11 +98,20 @@ function LandingPage() {
       <br />
       <br />
       <Container className="dark rounded text-center">
-        <Row>
-          <CurrentStatus name={"Carbondioxide"} {...co2Event} />
-          <CurrentStatus name={"Humidity"} {...humidityEvent} />
-          <CurrentStatus name={"Temperature"} {...temperatureEvent} />
-        </Row>
+        {error ? (
+          <Row>
+            <Col md>
+              <h1>Sorry, no Blockchain connection available</h1>
+              <p>Come back later or contact the publishers</p>
+            </Col>
+          </Row>
+        ) : (
+          <Row>
+            <CurrentStatus name={"Carbondioxide"} {...co2Event} />
+            <CurrentStatus name={"Humidity"} {...humidityEvent} />
+            <CurrentStatus name={"Temperature"} {...temperatureEvent} />
+          </Row>
+        )}
       </Container>
     </>
   );
